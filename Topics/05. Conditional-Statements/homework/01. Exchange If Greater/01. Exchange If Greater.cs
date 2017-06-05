ee87102cbd86f32bbf12b1e6548ad137bc63c1e1
MonikaTzenova <monikatzenova@gmail.com>
2017-06-05 22:58:29 +0300

    Create 01. Exchange If Greater.cs

---
+++ Topics/05. Conditional-Statements/homework/01. Exchange If Greater/01. Exchange If Greater.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Exchange_numbers
+{
+    class Exchange_numbers
+    {
+        static void Main()
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            if (a > b)
+            {
+                Console.WriteLine("{0} {1}", b, a);
+            }
+            else {
+                Console.WriteLine("{0} {1}", a, b);
+
+            }
+        }
+    }
+}

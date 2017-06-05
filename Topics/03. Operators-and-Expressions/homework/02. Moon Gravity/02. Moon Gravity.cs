f08e48271e9958b82651431837d420d14e671e81
MonikaTzenova <monikatzenova@gmail.com>
2017-06-05 22:00:58 +0300

    Create 02. Moon Gravity.cs

---
+++ Topics/03. Operators-and-Expressions/homework/02. Moon Gravity/02. Moon Gravity.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace MoonGravity
+{
+    class MoonGravity
+    {
+        static void Main()
+        {
+            double earthWeight = double.Parse(Console.ReadLine());
+            double moonWeight = earthWeight * 0.17;
+            Console.WriteLine("{0:0.000}", moonWeight);
+        }
+    }
+}

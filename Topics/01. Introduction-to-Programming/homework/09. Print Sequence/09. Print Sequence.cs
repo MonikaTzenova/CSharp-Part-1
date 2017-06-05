2fb2b196ddf4ff0bb6e604239d557010a24e0cc3
MonikaTzenova <monikatzenova@gmail.com>
2017-06-05 21:44:06 +0300

    Create 09. Print Sequence.cs

---
+++ Topics/01. Introduction-to-Programming/homework/09. Print Sequence/09. Print Sequence.cs
@@ -0,0 +1,21 @@
+using System;
+
+class PrintSequence
+{
+    static void Main()
+    {
+        int numberToPrint;
+        for (int i = 2; i < 12; i++)
+        {
+            if (i % 2 == 0)
+            {
+                numberToPrint = i;
+            }
+            else
+            {
+                numberToPrint = i * (-1);
+            }
+            Console.WriteLine(numberToPrint);
+        }
+    }
+}

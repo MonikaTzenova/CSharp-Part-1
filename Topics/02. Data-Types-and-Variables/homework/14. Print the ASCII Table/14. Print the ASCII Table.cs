ef625f19a23ac66daa7fd22f6a22662b383970d0
MonikaTzenova <monikatzenova@gmail.com>
2017-06-05 21:49:35 +0300

    Update 14. Print the ASCII Table.cs

--- Topics/02. Data-Types-and-Variables/homework/14. Print the ASCII Table/14. Print the ASCII Table.cs
+++ Topics/02. Data-Types-and-Variables/homework/14. Print the ASCII Table/14. Print the ASCII Table.cs
@@ -1,27 +1,15 @@
 using System;
 
-class ComparingFloats {
+class PrinttheASCIITable
+{
+    static void Main()
+    {
 
-  static void Main() {
-  
-  
-    double eps = 0.000001;
-    double a = double.Parse(Console.ReadLine());
-    double b = double.Parse(Console.ReadLine());
-    double difference = Math.Abs(a - b);
-    
-    if (difference <= eps) {
-    
-      Console.WriteLine("true");
-      
-    }
-    else {
-    
-    	Console.WriteLine("false");
-      
-    }
-    
-    
-  }
 
+        for (byte counter = 33; counter < 127; counter++)
+        {
+            Console.Write((char)counter);
+        }
+
+    }
 }

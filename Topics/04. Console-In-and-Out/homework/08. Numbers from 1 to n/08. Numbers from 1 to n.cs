730868eb261ed96887bc86ea0b625476bda6ffa6
MonikaTzenova <monikatzenova@gmail.com>
2017-06-05 22:33:04 +0300

    Create 08. Numbers from 1 to n.cs

---
+++ Topics/04. Console-In-and-Out/homework/08. Numbers from 1 to n/08. Numbers from 1 to n.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace NumbersToN
+{
+    class NumbersToN
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i ++) {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
